2ab7dc43c0c5a1c03f7a430f0af63c20c6119a1a
theoretician <joseoh@minimalister.com>
2019-02-10 10:25:39 -0800

    Updates dictionary key names and removed some abbreviations

--- soccer-league-coordinator.playground/Contents.swift
+++ soccer-league-coordinator.playground/Contents.swift
@@ -9,47 +9,47 @@ let player: [[String: Any]] = []
 
 /* Complete info of all 18 players in seperate Dictionary */
 
-let player1: [String: Any] = ["Name" : "Joe Smith", "Height" : 42, "Experience" : true, "Guardians" : "Jim and Jan Smith"]
+let player1: [String: Any] = ["Name" : "Joe Smith", "Height" : 42, "isExperienced" : true, "Guardians" : "Jim and Jan Smith"]
 
-let player2: [String: Any] = ["Name": "Jill Tanner", "Height": 36, "Experience": true, "Guardians": "Clara Tanner"]
+let player2: [String: Any] = ["Name": "Jill Tanner", "Height": 36, "isExperienced": true, "Guardians": "Clara Tanner"]
 
-let player3: [String: Any] = ["Name": "Bill Bon", "Height": 43, "Experience": true, "Guardians": "Sara and Jenny Bon"]
+let player3: [String: Any] = ["Name": "Bill Bon", "Height": 43, "isExperienced": true, "Guardians": "Sara and Jenny Bon"]
 
-let player4: [String: Any] = ["Name": "Eva Gordon","Height": 45, "Experience": false, "Guardians": "Wendy and Mike Gordon"]
+let player4: [String: Any] = ["Name": "Eva Gordon","Height": 45, "isExperienced": false, "Guardians": "Wendy and Mike Gordon"]
 
-let player5: [String: Any] = ["Name":"Matt Gill", "Heihgt": 40, "Experience": false, "Guardians": "Charles and Sylvia Gill"]
+let player5: [String: Any] = ["Name":"Matt Gill", "Heihgt": 40, "isExperienced": false, "Guardians": "Charles and Sylvia Gill"]
 
-let player6: [String: Any] = ["Name":"Kimmy Stein", "Heihgt": 41, "Experience": false, "Guardians": "Bill and Hillary Stein"]
+let player6: [String: Any] = ["Name":"Kimmy Stein", "Heihgt": 41, "isExperienced": false, "Guardians": "Bill and Hillary Stein"]
 
-let player7: [String: Any] = ["Name":"Sammy Adams", "Heihgt": 45, "Experience": false, "Guardians": "Jeff Adams"]
+let player7: [String: Any] = ["Name":"Sammy Adams", "Heihgt": 45, "isExperienced": false, "Guardians": "Jeff Adams"]
 
-let player8: [String: Any] = ["Name":"Karl Saygan", "Heihgt": 42, "Experience": true, "Guardians": "Heather Bledsoe"]
+let player8: [String: Any] = ["Name":"Karl Saygan", "Heihgt": 42, "isExperienced": true, "Guardians": "Heather Bledsoe"]
 
-let player9: [String: Any] = ["Name":"Suzane Greenberg", "Heihgt": 44, "Experience": true, "Guardians": "Henrietta Dumas"]
+let player9: [String: Any] = ["Name":"Suzane Greenberg", "Heihgt": 44, "isExperienced": true, "Guardians": "Henrietta Dumas"]
 
-let player10: [String: Any] = ["Name":"Sal Dali", "Heihgt": 41, "Experience": false, "Guardians": "Gala Dali"]
+let player10: [String: Any] = ["Name":"Sal Dali", "Heihgt": 41, "isExperienced": false, "Guardians": "Gala Dali"]
 
-let player11: [String: Any] = ["Name":"Joe Kavalier", "Heihgt": 39, "Experience": false, "Guardians": "Sam and Elaine Kavalier"]
+let player11: [String: Any] = ["Name":"Joe Kavalier", "Heihgt": 39, "isExperienced": false, "Guardians": "Sam and Elaine Kavalier"]
 
-let player12: [String: Any] = ["Name":"Ben Finkelstein", "Heihgt" : 44, "Experience": false, "Guardians": "Aaron and Jill Finkelstein"]
+let player12: [String: Any] = ["Name":"Ben Finkelstein", "Heihgt" : 44, "isExperienced": false, "Guardians": "Aaron and Jill Finkelstein"]
 
-let player13: [String: Any] = ["Name":"Diego Soto", "Heihgt": 41, "Experience": true, "Guardians": "Robin and Sarika Soto"]
+let player13: [String: Any] = ["Name":"Diego Soto", "Heihgt": 41, "isExperienced": true, "Guardians": "Robin and Sarika Soto"]
 
-let player14: [String: Any] = ["Name":"Chloe Alaska", "Heihgt": 47, "Experience": false, "Guardians": "David and Jamie Alaska"]
+let player14: [String: Any] = ["Name":"Chloe Alaska", "Heihgt": 47, "isExperienced": false, "Guardians": "David and Jamie Alaska"]
 
-let player15: [String: Any] = ["Name":"Arnold Willis", "Heihgt": 43, "Experience": false, "Guardians": "Claire Willis"]
+let player15: [String: Any] = ["Name":"Arnold Willis", "Heihgt": 43, "isExperienced": false, "Guardians": "Claire Willis"]
 
-let player16: [String: Any] = ["Name":"Phillip Helm", "Heihgt": 44, "Experience": true, "Guardians": "Thomas Helm and Eva Jones"]
+let player16: [String: Any] = ["Name":"Phillip Helm", "Heihgt": 44, "isExperienced": true, "Guardians": "Thomas Helm and Eva Jones"]
 
-let player17: [String: Any] = ["Name":"Les Clay", "Heihgt": 42, "Experience": true, "Guardians": "Wynonna Brown"]
+let player17: [String: Any] = ["Name":"Les Clay", "Heihgt": 42, "isExperienced": true, "Guardians": "Wynonna Brown"]
 
-let player18: [String: Any] = ["Name":"Herschel Krustofski", "Heihgt": 45, "Experience": true, "Guardians": "Hyman and Rachel Krustofski"]
+let player18: [String: Any] = ["Name":"Herschel Krustofski", "Heihgt": 45, "isExperienced": true, "Guardians": "Hyman and Rachel Krustofski"]
 
 
 
 // Collection includes all 18 players data
 
-var players = [player1, player2, player3, player4, player5,player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18]
+var totalPlayers = [player1, player2, player3, player4, player5,player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18]
 
 
 // empty collection to store expereince players and notexperiences players.
@@ -59,9 +59,9 @@ var notexperiencedPlayers: [[String:Any]] = []
 
 
 // Logic to store experienced players and not experienced players
-for player in players {
-    if let someExperienced = player["Experience"] as? Bool {
-        if someExperienced == true {
+for player in totalPlayers {
+    if let isPlayerExperienced = player["isExperienced"] as? Bool {
+        if isPlayerExperienced == true {
             experiencedPlayers.append(player)
         } else {
             notexperiencedPlayers.append(player)
@@ -81,14 +81,7 @@ var teamRaptors: [[String:Any]] = []
 var theSoccerLeague = [teamDragons, teamRaptors, teamSharks]
 
 
-// No.of players in each team.
-
-var totalPlayersPerTeam = (players.count / theSoccerLeague.count)
-
-
 // Logic to sort and store experienced players
-
-var expPlayerPerTeam = (experiencedPlayers.count / theSoccerLeague.count)
 
 for player in experiencedPlayers {
     if teamDragons.count < teamSharks.count {
@@ -102,8 +95,6 @@ for player in experiencedPlayers {
 
 
 // Login to sort and store not experienced players
-
-var notExpPlayersPerTeam = (notexperiencedPlayers.count / theSoccerLeague.count)
 
 for player in notexperiencedPlayers {
     if teamDragons.count < teamSharks.count {
